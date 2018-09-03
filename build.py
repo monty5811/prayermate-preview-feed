e53f3532b05ae5a01529db5b5e9556b14f2138ce
Dean Montgomery <montgomery.dean97@gmail.com>
2018-09-03 20:09:22 +0100

    customise theme colour, optionally only render future points

--- build.py
+++ build.py
@@ -1,4 +1,5 @@
 import os
+import datetime as dt
 
 from jinja2 import Environment, FileSystemLoader
 import requests
@@ -11,12 +12,29 @@ def load_feed():
     return data
 
 
+def only_future(data):
+    today = dt.date.today()
+    petitions = []
+    for petition in data["petitions"]:
+        p_date = dt.datetime.strptime(petition["date"], "%Y-%m-%d").date()
+        if p_date >= today:
+            petitions.append(petition)
+
+    data["petitions"] = petitions
+    return data
+
+
 def build():
     env = Environment(loader=FileSystemLoader("."))
 
     path_to_tmpl = os.path.join("index.html.tmpl")
     data = load_feed()
-    result = env.get_template(path_to_tmpl).render(data=data)
+    if os.environ.get("ONLY_FUTURE", "False") == "True":
+        only_future(data)
+
+    result = env.get_template(path_to_tmpl).render(
+        data=data, theme_colour=os.environ.get("THEME_COLOUR", False)
+    )
 
     with open("index.html", "w") as f:
         f.write(result)
